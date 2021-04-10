74343f56b535e7cb1274d71672abe4f6437bf6c0
Latitude Brown <34399933+LatitudeAmpere@users.noreply.github.com>
2021-04-10 18:14:45 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   info: {
     name: "invite",
-    description: "invites bot to your server",
+    description: "Invites the Botler to come work at your server",
     usage: "[invite]",
     aliases: ["inv"],
   },
@@ -14,7 +14,7 @@ module.exports = {
     
     let invite = new MessageEmbed()
     .setTitle(`Invite ${client.user.username}`)
-    .setDescription(`Invite me to your server flavor text  \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
+    .setDescription(`Hire me to work at your discord server!  \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
     .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`)
     .setColor("BLUE")
     return message.channel.send(invite);
