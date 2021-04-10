4552e757d135d6ff080ba6d35097b116f6f09167
Latitude Brown <34399933+LatitudeAmpere@users.noreply.github.com>
2021-04-10 18:17:57 -0400

    nowplaying.js

---
+++ commands/nowplaying.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+const sendError = require("../util/error")
+
+module.exports = {
+  info: {
+    name: "nowplaying",
+    description: "Tells you what is currently playing",
+    usage: "[nowplaying]",
+    aliases: ["np"],
+  },
+
+  run: async function (client, message, args) {
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue) return sendError("There is nothing playing", message.channel);
+    let song = serverQueue.songs[0]
+    let thing = new MessageEmbed()
+      .setAuthor("Now Playing")
+      .setThumbnail(song.img)
+      .setColor("BLUE")
+      .addField("Name", song.title, true)
+      .addField("Duration", song.duration, true)
+      .addField("Requested by", song.req.tag, true)
+      .setFooter(`Views: ${song.views} | ${song.ago}`)
+    return message.channel.send(thing)
+  },
+};
