6f0a8a55f5114fed42a367d5424e27860b8a87f1
Latitude Brown <34399933+LatitudeAmpere@users.noreply.github.com>
2021-04-10 18:17:03 -0400

    lyrics.js

---
+++ commands/lyrics.js
@@ -0,0 +1,40 @@
+const { MessageEmbed } = require("discord.js");
+const lyricsFinder = require("lyrics-finder");
+const sendError = require("../util/error");
+const splitlyrics = require("../util/pagination");
+
+module.exports = {
+    info: {
+        name: "lyrics",
+        description: "fetches the lyrics for you",
+        usage: "[lyrics]",
+        aliases: ["ly"],
+    },
+
+    run: async function (client, message, args) {
+        const queue = message.client.queue.get(message.guild.id);
+        if (!queue) return sendError("nothing playing", message.channel).catch(console.error);
+
+        let lyrics = null;
+
+        try {
+            lyrics = await lyricsFinder(queue.songs[0].title, "");
+            if (!lyrics) lyrics = `I could not find the lyrics for ${queue.songs[0].title}.`;
+        } catch (error) {
+            lyrics = `I could not find the lyrics for ${queue.songs[0].title}.`;
+        }
+        const splittedLyrics = splitlyrics.chunk(lyrics, 1024);
+
+        let lyricsEmbed = new MessageEmbed()
+            .setAuthor(`${queue.songs[0].title} — Lyrics`)
+            .setThumbnail(queue.songs[0].img)
+            .setColor("YELLOW")
+            .setDescription(splittedLyrics[0])
+            //terrible var names
+            .setFooter(`Page 1 of ${splittedLyrics.length}.`)
+            .setTimestamp();
+
+        const lyricsMsg = await message.channel.send(lyricsEmbed);
+        if (splittedLyrics.length > 1) await splitlyrics.pagination(lyricsMsg, message.author, splittedLyrics);
+    },
+};
